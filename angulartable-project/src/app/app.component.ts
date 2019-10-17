d43dacb79576162af4e7c927f129b2ee200dcc2d
uhamadhuri <uhamadhuri95@gmail.com>
2019-10-17 21:24:21 +0530

    creation of table by using ajax in angular

---
+++ angulartable-project/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import * as  $ from "jquery" ;
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'angulartable-project';
+
+
+  ngOnInit(){
+  var  that=this;
+    $.ajax({
+      url:"https://jsonplaceholder.typicode.com/users",
+      method:"GET",
+      success:function(res){
+           console.log(res)
+          that.users=res;  
+         
+          
+      }
+
+  })
+
+}
+}
+
